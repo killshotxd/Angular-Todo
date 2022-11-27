0c102e31a0fdd04a81db1ded14eb73ea71e4967b
killshotxd <hassanansari211@gmail.com>
2022-11-27 11:30:46 +0530

    initializing bootstrap and fa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AngularFireModule, AngularFirestoreModule],
+  declarations: [AppComponent, TodoComponent],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDr5vhZCwhXF-LE9yS0NrYtI-M5s8VAuLA',
+    authDomain: 'chitchat-c83aa.firebaseapp.com',
+    projectId: 'chitchat-c83aa',
+    storageBucket: 'chitchat-c83aa.appspot.com',
+    messagingSenderId: '682893125388',
+    appId: '1:682893125388:web:4a183b9db85999d82e4b29',
+    measurementId: 'G-SKEJZ4CHWS',
+  },
 };
