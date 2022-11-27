53e586891018bc51adf824771d2e4342aba453cb
killshotxd <hassanansari211@gmail.com>
2022-11-27 11:21:27 +0530

    Initializing Firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AngularFireModule, AngularFirestoreModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDr5vhZCwhXF-LE9yS0NrYtI-M5s8VAuLA',
+    authDomain: 'chitchat-c83aa.firebaseapp.com',
+    projectId: 'chitchat-c83aa',
+    storageBucket: 'chitchat-c83aa.appspot.com',
+    messagingSenderId: '682893125388',
+    appId: '1:682893125388:web:4a183b9db85999d82e4b29',
+    measurementId: 'G-SKEJZ4CHWS',
+  },
 };
 
 /*
